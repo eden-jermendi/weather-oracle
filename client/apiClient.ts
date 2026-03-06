f19878835175c945f1d9dbffe76d6fe38c96c92c
Carol Tang <catang824@gmail.com>
2026-03-06 23:03:19 +1300

    make weatherAPI call from meteoblue

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,8 +1,22 @@
 import request from 'superagent'
 
-const rootURL = new URL(`/api/v1`, document.baseURI)
+const rootURL = new URL(`/v1`, document.baseURI)
 
-export async function getGreeting() {
-  const res = await request.get(`${rootURL}/greeting`)
-  return res.body.greeting as string
+// export async function getGreeting() {
+//   const res = await request.get(`${rootURL}/greeting`)
+//   return res.body.greeting as string
+// }
+
+export type WeatherData = {
+  temperature: number
+  windSpeed: number
+  cloudiness: number
+  precipitation: number
+  pictocode: number
+  isDaylight: boolean
+}
+
+export async function getWeather(lat: number, lon: number) {
+  const res = await request.get(`${rootURL}/weather`).query({ lat, lon })
+  return res.body as WeatherData
 }
